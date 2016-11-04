23e474d5359265701e67eb6b1c9e0b2fbfd902e1
Adam Lockwood <alockwood@clearvision-cm.com>
2016-11-04 16:07:09 +0000

    messed around with visual for lunch

--- main.js
+++ main.js
@@ -17,7 +17,15 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    minWidth: 750,
+    minHeight: 510,
+    darkTheme: true,
+  })
+
+  //frame: false
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
